c46068324596dba6595a0e8806c304b36b82d73a
Cleiton Lahm <cleijllahm@gmail.com>
2023-05-05 12:32:39 -0300

    finish

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
-  $('header button').click(function() {
-    $('form').slideDown();
-})
+  $('header button').click(function () {
+    $('form').slideDown()
+  })
 
-$('#botao-cancelar').click(function() {
-  $('form').slideUp();
-})
+  $('#botao-cancelar').click(function () {
+    $('form').slideUp()
+  })
 
   $('form').on('submit', function (e) {
-    e.preventDefault();
-  })
+    e.preventDefault()
+    const enderecoNovaImagem = $('#endereco-imagem-nova').val()
+    const novoItem = $('<li style="display:none"></li>')
+    $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem)
+    
+    $(`
+
+    <div class="overlay-imagem-link">
+      <a href="${enderecoNovaImagem}" target="_blanck" tittle="Ver imagem em tamanho real"
+        Ver imagem em tamanho real
+    </div
+    
+    `).appendTo(novoItem)
+    $(novoItem).appendTo('ul')
+    $(novoItem).fadeIn()
+    $('#endereco-imagem-nova').val("")
+})
 })
